1d6da30971148119728606b8a896e80a78c39e6f
Chirag Parmar <95179516+chiragxp@users.noreply.github.com>
2025-04-10 23:55:28 -0400

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col text-center h-screen justify-center">
+        <h1 className="text-3xl">Hello there!</h1>
+        <h2 className="text-2xl">
+          The website is currently under construction.
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
